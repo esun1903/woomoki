974c0a94bd727ecc2f543cc40b04d12af018d8e9
pyoki32 <pyoki32@gmail.com>
2021-01-20 15:42:56 +0900

    etc/master/dir 초기설정 test

---
+++ backend/src/main/java/com/ssafy/project/HomeController.java
@@ -0,0 +1,13 @@
+package com.ssafy.project;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+	@GetMapping(value = "/")
+	private String home() {
+		return "index.html";
+	}
+}
---
+++ frontend/src/views/Join/Join.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Join',
+    components: { ComponentName },
+    directives: { DirectiveName },
+    data() {
+        return {
+            
+        };
+    },
+    mounted() {
+        
+    },
+    methods: {
+        
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
