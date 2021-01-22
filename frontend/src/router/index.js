8c6acdefcc4e3ab031ab727b235b56d4c17e0f93
Hyojin Kim <gywld07@naver.com>
2021-01-22 16:02:55 +0900

    [S04P12A303-161] feat/hyo/navbar+join/Navbar: Create

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,10 +1,16 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 // import Home from "../views/Home.vue";
+import Join from "@/views/Join/Join.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
+  {
+    path: "/join",
+    name: "Join",
+    component: Join
+  },
   // {
   //   path: "/about",
   //   name: "About",
