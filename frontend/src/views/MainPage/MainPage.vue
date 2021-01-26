eb89e8852644635cda838b0d8db2c18c851951a8
Hyojin Kim <gywld07@naver.com>
2021-01-26 17:28:19 +0900

    [S04P12A303-272] feat/hyo/hyo/MainPage/MainPage : Create MainPage

---
+++ frontend/src/views/MainPage/MainPage.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <SearchBar/>
+  </div>
+</template>
+
+<script>
+import SearchBar from "@/views/MainPage/components/SearchBar.vue"
+export default {
+  name: 'MainPage',
+  components: { SearchBar },
+  directives: {  },
+  data() {
+    return {
+      
+    };
+  },
+  mounted() {
+    
+  },
+  methods: {
+    
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ frontend/src/views/MainPage/components/SearchBar.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="search-wrapper">
+      <div class="input-holder">
+          <input type="text" class="search-input" placeholder="Type to search" />
+          <button class="search-icon" onclick="searchToggle(this, event);"><span></span></button>
+      </div>
+      <span class="close" onclick="searchToggle(this, event);"></span>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'SearchBar',
+  components: {  },
+  directives: {  },
+  data() {
+    return {
+      
+    };
+  },
+  mounted() {
+    
+  },
+  methods: {
+    
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.search-wrapper {
+    position: absolute;
+    transform: translate(-50%, -50%);
+    top:50%;
+    left:50%;
+}
+.search-wrapper.active {}
+
+</style>
