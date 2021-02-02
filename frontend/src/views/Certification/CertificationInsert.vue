915f8a96612526b5c42edadf8a234cd2649c06f1
JiHee_Hong <vegavenus0920@gmail.com>
2021-02-02 12:54:56 +0900

    [S04P13A303-69] feat/Hongji/Certification : Update File upload func

--- frontend/src/views/Certification/CertificationInsert.vue
+++ frontend/src/views/Certification/CertificationInsert.vue
@@ -1,6 +1,20 @@
 <template>
-    <v-file-input label="인증사진" outlined multiple small-chips dense :rules="rules"
+<v-app id="temp">
+     <v-container fluid>
+            <v-layout wrap>
+                 <v-flex xs12 sm4>
+    <v-file-input label="인증사진" outlined multiple dense :rules="rules"
         accept="image/png, image/jpeg, image/bmp, image/jpg" prepend-icon="mdi-camera"></v-file-input>
+
+                    <v-combobox multiple v-model="select" label="Tags" small-chips deletable-chips
+                        class="tag-input" :search-input.sync="search">
+                    </v-combobox>
+                </v-flex>
+
+            </v-layout>
+        </v-container>
+    </v-app>
+
 </template>
 
 <script>
@@ -9,19 +23,41 @@
         components: {},
         directives: {},
         data() {
-            return {
+    return {
+      select: [],
+      items: [],
+      search: "" //sync search
+    };
+  },
+  mounted() {
 
-            };
         },
-        mounted() {
+  methods: {
 
-        },
-        methods: {
-
-        },
+  }
     };
 </script>
 
 <style lang="scss" scoped>
 
+#temp{
+    margin-top:10%;
+}
+
+
+.tag-input span.v-chip::before {
+    content: "label";
+    font-weight: normal;
+    font-style: normal;
+    font-size: 20px;
+    line-height: 1;
+    letter-spacing: normal;
+    text-transform: none;
+    display: inline-block;
+    white-space: nowrap;
+    word-wrap: normal;
+    direction: ltr;
+    -webkit-font-feature-settings: 'liga';
+    -webkit-font-smoothing: antialiased;
+}
 </style>
