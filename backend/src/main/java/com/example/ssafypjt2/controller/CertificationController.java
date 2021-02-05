6f43036b4cdcccca338b0b33d658496dcc31018a
esun1903 <dmstjs1996@naver.com>
2021-02-05 20:32:04 +0900

    [S04P13A303-66] feat/choi/challengeList/Update main: 에러수정

--- backend/src/main/java/com/example/ssafypjt2/controller/CertificationController.java
+++ backend/src/main/java/com/example/ssafypjt2/controller/CertificationController.java
@@ -18,6 +18,7 @@ import com.example.ssafypjt2.service.CertificationService;
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 public class CertificationController {
+	// test
 
 	@Autowired
 	private CertificationService certificationService;
