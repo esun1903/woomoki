5bd3c5a9fb0dbf15cc8dae8ab9d5c33c8d5b8133
dydalsdl78 <dydalsdl78@gmail.com>
2021-02-07 21:15:44 +0900

    Feat/yong/SeedDetail : Create SeedDetail

---
+++ frontend/src/views/SeedDetail/SeedDetail.vue
@@ -0,0 +1,61 @@
+<template>
+  <v-container>
+    
+    <v-row>
+      <SeedThumbnail></SeedThumbnail>
+    </v-row>
+
+    <v-row>
+
+      <v-tabs
+        v-model="tab"
+        color="deep-purple accent-4"
+        right
+      >
+        <v-tab
+          v-for="item in items"
+          :key="item"
+          >
+          {{ item }}
+        </v-tab>
+      </v-tabs>
+      <v-tabs-items v-model="tab">
+        <v-tab-item
+          v-for="item in items"
+          :key="item"
+          >
+          <v-card>
+            <v-card-text v-if="item === '씨앗 정보'"><SeedBasicInfo></SeedBasicInfo></v-card-text>
+            <v-card-text class="d-flex justify-center" v-if="item === '보살핌 후기'"><SeedCertification></SeedCertification></v-card-text>
+          </v-card>
+        </v-tab-item>
+      </v-tabs-items>
+
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import SeedThumbnail from "./components/SeedThumbnail"
+import SeedBasicInfo from "./components/SeedBasicInfo"
+import SeedCertification from "./components/SeedCertification"
+
+export default {
+  name: "SeedDetail",
+  components: {
+    SeedThumbnail,
+    SeedBasicInfo,
+    SeedCertification
+  },
+  data: function () {
+    return {
+      tab: null,
+      items: ["씨앗 정보", "보살핌 후기"],
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
