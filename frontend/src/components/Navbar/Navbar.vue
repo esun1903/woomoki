fa5e1926d0ec7d5b38f9017f4112dcd2d2de89eb
Hyojin Kim <gywld07@naver.com>
2021-02-11 01:21:14 +0900

    [S04P13A303-38,81,97] /hyo/Search/Search: Update Search

--- frontend/src/components/Navbar/Navbar.vue
+++ frontend/src/components/Navbar/Navbar.vue
@@ -1,18 +1,16 @@
 <template>
     <div class="navbar-block"> 
       <v-app-bar class="navbar" fixed>
-        <router-link to="/">
+        <router-link to="/" class="router">
           <v-img 
             width="4vw"
             class="mt-3"
             :src="require('@/assets/images/logo.png')"
           ></v-img>
         </router-link>
-        <v-spacer></v-spacer>
         <SearchBar/>
-        <v-spacer></v-spacer>
 
-        <div class="btn_group">
+        <div class="btn-group">
           <v-menu offset-y open-on-hover bottom left>
             <template v-slot:activator="{ on, attrs }">
               <v-btn icon v-bind="attrs" v-on="on" class="btn">
@@ -40,30 +38,30 @@
           <v-btn icon class="btn" @click="notice = true">
             <v-icon>mdi-bell-ring</v-icon>
           </v-btn>
-          <v-dialog v-model="notice">
-          <div class="notification">
-            <v-card class="notice-card">
-              <v-tabs v-model="currentTab" grow dark>
-                <v-tabs-slider></v-tabs-slider>
-                <v-tab v-for="tab in tabs" :key="tab.title">
-                  {{ tab.title }}
-                </v-tab>
-              </v-tabs>
+          <v-dialog v-model="notice" width="25%">
+            <div class="notification">
+              <v-card class="notice-card">
+                <v-tabs v-model="currentTab" grow dark>
+                  <v-tabs-slider></v-tabs-slider>
+                  <v-tab v-for="tab in tabs" :key="tab.title">
+                    {{ tab.title }}
+                  </v-tab>
+                </v-tabs>
 
-              <v-tabs-items v-model="currentTab">
-                <v-tab-item v-for="tab in tabs" :key="tab.title">
-                  <v-card flat>
-                    <v-card-text>{{ tab.content }}</v-card-text>
-                  </v-card>
-                </v-tab-item>
-              </v-tabs-items>
-              <v-card-actions>
-                <v-btn color="primary" text @click="notice = false">
-                  Close
-                </v-btn>
-              </v-card-actions>
-            </v-card>
-          </div>
+                <v-tabs-items v-model="currentTab">
+                  <v-tab-item v-for="tab in tabs" :key="tab.title">
+                    <v-card flat>
+                      <v-card-text>{{ tab.content }}</v-card-text>
+                    </v-card>
+                  </v-tab-item>
+                </v-tabs-items>
+                <v-card-actions>
+                  <v-btn color="primary" text @click="notice = false">
+                    Close
+                  </v-btn>
+                </v-card-actions>
+              </v-card>
+            </div>
           </v-dialog>
           
           <v-menu offset-y open-on-hover bottom>
@@ -74,10 +72,7 @@
             </template>
             <v-list>
               <div v-if="getCheckLogin">
-                <!-- <v-list-item @click="goMyPage"> -->
                 <v-list-item>
-                  <!-- query사용 -->
-                  <!-- <router-link :key="$route.fullPath" :to="{ name: 'UserPage', query: { userNickname: userNickname }}"> -->
                   <router-link :key="$route.fullPath" :to="{ name: 'UserPage', params: { userNickname: userNickname }}">
                     <v-list-item-title>마이페이지</v-list-item-title>
                    </router-link>
@@ -136,10 +131,6 @@ export default {
     }
   },  
   methods: {
-    
-    // goMyPage: function () {
-    //   this.$store.dispatch("UserStore/compareId", this.$store.state.UserStore.user.user_id);
-    // },
     goLogout: function () {
       this.$store.dispatch('UserStore/logOut')
     },
@@ -172,9 +163,16 @@ a:-webkit-any-link {
     text-decoration: none;
 }
 
+.btn-group {
+  padding-left: 20%;
+}
 
 .navbar-block {
   margin-bottom: 5%;
+}
+
+.router {
+  padding-right:30%;;
 }
 
 .navbar {
@@ -190,7 +188,7 @@ a:-webkit-any-link {
   }
 }
 .v-dialog > * {
-  width: 25%;
+  width: 100%;
 }
 .v-tabs-slider {
   color: red;
@@ -198,5 +196,7 @@ a:-webkit-any-link {
 .v-dialog__content {
   display: flex;
   align-items: flex-start;
+  justify-content: flex-end;
+  margin-top: 2%;
 }
 </style>
--- frontend/src/views/SearchedPage/SearchedPage.vue
+++ frontend/src/views/SearchedPage/SearchedPage.vue
@@ -1,31 +1,103 @@
 <template>
-  <div>
-    {{ $route.query.id }}
-  </div>
+  <v-container>
+    <div class="searched-keyword">
+      <p>
+        <span>{{ searched }}</span>
+        에 관련된 검색 결과입니다.
+      </p>
+    </div>
+    <div class="cards">
+      <v-row dense>
+        <v-col cols="3" class="card" v-for="(seed, index) in seeds" :key="index">
+          <SeedCard :seed="seed"/>
+        </v-col>
+      </v-row>
+    </div>
+  </v-container>
 </template>
 
 <script>
+import SeedCard from "@/views/MainPage/components/SeedCard.vue";
+import axios from "axios";
+import {mapState} from "vuex";
 export default {
   name: 'SearchedPage',
-  components: {  },
+  components: { SeedCard },
   directives: {  },
   data() {
     return {
-      searched: this.$route.query.id
+      searched: this.$route.query.keyword,
+      seeds: []
     };
   },
   mounted() {
     
   },
+  computed: {
+   ...mapState('UserStore', ['user']),
+  },
   methods: {
-    
+    arrangeRule: function (val) {
+      axios.get("http://localhost:8080/allChallenge")
+        .then((res) => {
+          const seeds = res.data
+          const searchedSeeds=[]
+          for (const i in seeds) {
+            const seed = seeds[i]
+            const seedTitle = seed["title"]
+            if (seedTitle.indexOf(val) != -1) {
+              searchedSeeds.push(seed)
+            }
+          }
+          this.seeds = searchedSeeds
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+    }  
   },
   created () {
-
+  },
+  watch: {
+    '$route' () {
+      this.searched = this.$route.query.keyword
+      this.arrangeRule(this.searched)
+    },
   }
 };
 </script>
 
 <style lang="scss" scoped>
-
+.container {
+  width: 100%;
+  height: 89.5vh;
+  margin-bottom: 10%;
+  .searched-keyword {
+    display: flex;
+    justify-content: center;
+    margin: 1% 0;
+    p {
+      font-size: 1.5em;
+      font-weight: bold;
+      span {
+        font-size: 1.2em;
+      }
+    }
+  }
+  .cards {
+    width: 100%;
+    height: 100%;
+    .row {
+      .card {
+        padding: 1%;
+        width: 100%;
+        height: 50%;
+        .v-card {
+          width: 100%;
+          height: 50%;
+        }
+      }    
+    }
+  }
+}
 </style>
