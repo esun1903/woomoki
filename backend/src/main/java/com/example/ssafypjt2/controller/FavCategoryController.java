c6eacf45093c2971502c17e53825dcabd524740b
pyoki32 <pyoki32@gmail.com>
2021-02-12 02:23:01 +0900

    [S04P13A303-165]feat/pyoki/favCategoryList add nickname으로 favcategory List 불러오기

--- backend/src/main/java/com/example/ssafypjt2/controller/FavCategoryController.java
+++ backend/src/main/java/com/example/ssafypjt2/controller/FavCategoryController.java
@@ -5,16 +5,12 @@ import java.util.Locale.Category;
 
 import javax.servlet.http.HttpServletResponse;
 
+import com.example.ssafypjt2.dto.CategoryDto;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.example.ssafypjt2.dto.FavCategoryDto;
 import com.example.ssafypjt2.dto.UserDto;
@@ -29,19 +25,19 @@ public class FavCategoryController {
 
 	@CrossOrigin(origins = "*") // 특정 도메인만 접근을 허용해 주는 어노테이션
 	@PostMapping("/signup/favCategory")
-	public int favCategory(@RequestBody FavCategoryDto [] favCategoryDto, HttpServletResponse response) { // 회원가입 post 요청
+	public int favCategory(@RequestBody FavCategoryDto[] favCategoryDto, HttpServletResponse response) { // 회원가입 post 요청
 
 		int result = 0;
 		HttpStatus status = null;
 		try {
- 
+
 			for (FavCategoryDto list : favCategoryDto) {
-		
+
 				System.out.println("가장 좋아하는 카테고리 insert 하기 : " + list.getCategory_id()); // 가장 좋아하는 카테고리 고르기
 				result = favcategoryService.favCategory(list);
 				System.out.println(result);
 			}
-		
+
 
 		} catch (Exception e) {
 			logger.error("카테고리 등록 실패 : {}", e);
@@ -49,40 +45,45 @@ public class FavCategoryController {
 		}
 		return result;
 	}
-	
+
 	@CrossOrigin(origins = "*") // 특정 도메인만 접근을 허용해 주는 어노테이션
 	@PostMapping("/userPage/UpdatefavCategory")
-	public int updatefavCategory(@RequestBody FavCategoryDto [] favCategoryDto) { // 회원가입 post 요청
-      System.out.println("여기 오긴 하는거야?");
+	public int updatefavCategory(@RequestBody FavCategoryDto[] favCategoryDto) { // 회원가입 post 요청
+		System.out.println("여기 오긴 하는거야?");
 		int result = 0;
 		HttpStatus status = null;
 		try {
-            
-			
+
+
 			System.out.println(result);
 //			for (FavCategoryDto list : favCategoryDto) {
 //				System.out.println(list.getUser_id() +" 유저의 관심 카테고리 수정하기  : " + list.getCategory_id()); // 가장 좋아하는 카테고리 고르기
 //				result = favcategoryService.updatefavCategory(list);
 //				System.out.println(result);
 //			}
-			
+
 			for (int i = 0; i < favCategoryDto.length; i++) {
 				FavCategoryDto list = favCategoryDto[i];
-				if(i == 0) {
-					result = favcategoryService.deletefavCategory(list.getUser_id());					
+				if (i == 0) {
+					result = favcategoryService.deletefavCategory(list.getUser_id());
 				}
-				System.out.println(list.getUser_id() +" 유저의 관심 카테고리 수정하기  : " + list.getCategory_id()); // 가장 좋아하는 카테고리 고르기
+				System.out.println(list.getUser_id() + " 유저의 관심 카테고리 수정하기  : " + list.getCategory_id()); // 가장 좋아하는 카테고리 고르기
 				result = favcategoryService.updatefavCategory(list);
 				System.out.println(result);
 			}
-			
-		
+
 
 		} catch (Exception e) {
 			logger.error("카테고리 등록 실패 : {}", e);
 			status = HttpStatus.INTERNAL_SERVER_ERROR;
 		}
 		return result;
+	}
+
+	@CrossOrigin(origins = "*")
+	@GetMapping("/userPage/favCategoryName/{nickName}")
+	public List<CategoryDto> favCategoryName (@PathVariable String nickName){
+		return favcategoryService.favCategoryName(nickName);
 	}
 
 }
--- backend/src/main/java/com/example/ssafypjt2/dao/FavCategoryDao.java
+++ backend/src/main/java/com/example/ssafypjt2/dao/FavCategoryDao.java
@@ -1,5 +1,6 @@
 package com.example.ssafypjt2.dao;
 
+import com.example.ssafypjt2.dto.CategoryDto;
 import com.example.ssafypjt2.dto.ChallengeDto;
 import org.apache.ibatis.annotations.*;
 
@@ -26,6 +27,13 @@ public interface FavCategoryDao {
 
 	@Select("SELECT * FROM  challenge WHERE category_id = #{user_id} ")
 	public List<ChallengeDto> main_favCategory2(@Param("user_id") int user_id);
+
+	@Select("SELECT c.name FROM fav_category AS fc INNER JOIN category AS c "
+			+ "ON fc.category_id = c.id "
+			+ "INNER JOIN user u "
+			+ "ON fc.user_id = u.id "
+			+ "AND u.nickname = #{nickName}")
+	public List<CategoryDto> favCategoryName(@Param("nickName") String nickName);
 
 //	@Insert("INSERT INTO fav_category ( user_id, category_id ) VALUES ( #{favCategoryDto.user_id} , #{favCategoryDto.category_id} ) ")
 //	@Options(useGeneratedKeys = true) // myBatis 에서 자동생성키를 true를 했을 때 
