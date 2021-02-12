a45d6279df2dccf43a1916bb2052801c5bd9deb7
dydalsdl78 <dydalsdl78@gmail.com>
2021-02-12 17:39:00 +0900

    Feat/yong/SeedDetail : Update floating banner

--- frontend/src/views/SeedDetail/components/SeedCertification.vue
+++ frontend/src/views/SeedDetail/components/SeedCertification.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-row>
+  <!-- <v-row>
     <v-col
       v-for="(card, idx) in cards"
       :key="idx"
@@ -26,7 +26,44 @@
         </template>
       </v-img>
     </v-col>
-  </v-row>
+  </v-row> -->
+
+  <div>
+    <v-row>
+      <v-col
+        v-for="(card, $idx) in cards"
+        :key="$idx"
+        class="d-flex child-flex"
+        cols="4"
+      >
+      <v-img
+        :src="card.img"
+        :lazy-src="card.img"
+        aspect-ratio="1"
+        class="grey lighten-2 cursor_test"
+      >
+        <template v-slot:placeholder>
+          <v-row
+            class="fill-height ma-0"
+            align="center"
+            justify="center"
+          >
+            <v-progress-circular
+              indeterminate
+              color="grey lighten-5"
+            ></v-progress-circular>
+          </v-row>
+        </template>
+      </v-img>
+    </v-col>
+    </v-row>
+
+    <infinite-loading @infinite="infiniteHandler" spinner="waveDots">
+      <div slot="no-more">
+        데이터가 없습니다
+      </div>
+    </infinite-loading> 
+  </div>
 </template>
 
 <script>
@@ -40,6 +77,7 @@ export default {
   data: function () {
     return {
       seedId: this.$route.params.seedId,
+      total: [],
       cards: [],
     }
   },
@@ -49,13 +87,27 @@ export default {
       console.log(this.seedId)
       axios.get(`http://127.0.0.1:8080/sameChallengeCertification/${seedId}`)
         .then((res) => {
-          this.cards = res.data
+          this.total = res.data
           console.log("인증:",res.data)
         })
         .catch((err) => {
           console.log(err)
         })
     },
+    infiniteHandler($state) {
+        setTimeout(() => {
+        if (this.total.length) {
+          // console.log("옮겨지기 전 cards: ", this.cards)
+          // console.log("옮겨지기 전 total: ", this.total)
+          this.cards.push(...this.total.splice(0, 3))
+          console.log("옮겨진 후 cards: ", this.cards)
+          // console.log("옮겨진 후 total: ", this.total)
+          $state.loaded();
+        } else {
+          $state.complete();
+        }
+        }, 1000)
+      }
   },
   created() {
     this.getSeedCertification();
