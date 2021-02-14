ec17c59df18c08a436c8797b1b2d364123c95933
Hyojin Kim <gywld07@naver.com>
2021-02-14 20:37:00 +0900

    [S04P13A303-38] /hyo/Feed/Feed: Update Feed

--- frontend/src/views/Feed/components/FeedCard.vue
+++ frontend/src/views/Feed/components/FeedCard.vue
@@ -2,9 +2,9 @@
   <splide-slide>
     <div class="image-box">
       <img :src=CertificationImg>
-    </div>
-    <div class="mask">
-      내용 보이게
+      <div class="mask">
+        <div class="text">보이나??</div>
+      </div>
     </div>
   </splide-slide>
 </template>
@@ -37,25 +37,56 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-// .splide__slide {
-//   vertical-align: middle;
-  .image-box {
-    display:flex;
-    justify-content: center;
-    img {
-    width: 250px;
-    height: 300px;
-    object-fit: cover;
-      padding: 0 5%;
-      margin: 0;
-      &:hover + .mask{
-        display: block;
-        color: red;
-      }
+.image-box {
+  display:flex;
+  justify-content: center;
+  img {
+  width: 320px;
+  height: 300px;
+  object-fit: cover;
+  padding: 0 1%;
+  margin: 0;
+
+  display: block;
+  transition: .5s ease;
+  backface-visibility: hidden;
+    &:hover {
+      opacity: 0.8;
     }
+    // &:hover + .mask{
+    //   display: block;
+    //   color: red;
+    // }
   }
   .mask {
-    display: none;
+    transition: .5s ease;
+    opacity: 0;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    -ms-transform: translate(-50%, -50%);
+    text-align: center;
+    &:hover {
+      opacity: 1;
+    }
+    .text {
+      background-color: white;
+      color: black;
+      font-size: 16px;
+      padding: 16px 32px;
+    }
   }
+}
+.container:hover .image {
+  opacity: 0.3;
+}
+
+.container:hover .middle {
+  opacity: 1;
+}
+  // .mask {
+  //   display: none;
+  // }
 // 
 </style>
